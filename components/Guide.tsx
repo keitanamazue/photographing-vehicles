0709c90751f56ca439fe6f06d3d160a0cd129f0e
keitanamazue <keitanamazue@gmail.com>
2023-03-06 16:45:14 +0900

    border width height 修正・追加

--- components/Guide.tsx
+++ components/Guide.tsx
@@ -3,22 +3,36 @@ import React from "react";
 export const Guide = (props: { path: string }) => {
   const { path } = props;
   return (
-    <div
-      style={{
-        position: "absolute",
-        top: "50%",
-        left: "57%",
-        transform: "translateY(-50%) translateX(-50%) rotate(90deg)",
-        backgroundImage: `url(${path})`,
-        backgroundPosition: "center",
-        backgroundSize: "cover",
-        backgroundRepeat: "no-repeat",
-        width: "560px",
-        height: "260px",
-        margin: "0 auto",
-        opacity: "0.6",
-        filter: "sepia(300%) hue-rotate(150deg) saturate(450%)",
-      }}
-    />
+    <div>
+      <div
+        style={{
+          position: "absolute",
+          top: "48%",
+          left: "54%",
+          transform: "translateY(-50%) translateX(-50%) rotate(90deg)",
+          width: "380px",
+          height: "230px",
+          margin: "0 auto",
+          border: "solid 1px white",
+        }}
+      />
+      <div
+        style={{
+          position: "absolute",
+          top: "50%",
+          left: "57%",
+          transform: "translateY(-50%) translateX(-50%) rotate(90deg)",
+          backgroundImage: `url(${path})`,
+          backgroundPosition: "center",
+          backgroundSize: "cover",
+          backgroundRepeat: "no-repeat",
+          width: "450px",
+          height: "220px",
+          margin: "0 auto",
+          opacity: "0.6",
+          filter: "sepia(300%) hue-rotate(150deg) saturate(450%)",
+        }}
+      />
+    </div>
   );
 };
