f6715132216719c771e5998f49182a3df4cbf846
keitanamazue <keitanamazue@gmail.com>
2023-03-06 21:54:46 +0900

    朔郎

--- pages/take/light/normal/step/[stepId].tsx
+++
@@ -1,7 +0,0 @@
-/* eslint-disable react-hooks/rules-of-hooks */
-import React, { useState, useRef, useEffect } from "react";
-import { GuideContainer } from "../../../../../components/GuideContainer";
-
-export default function index() {
-  return <div>a</div>;
-}
